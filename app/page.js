ce787064df9bcb2a61a327c6740c54dd7933276d
ananyaw <annuwanchoo@gmail.com>
2026-08-25 10:30:13 -0400

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif", maxWidth: 480, margin: "0 auto" }}>
+      <h1>Health OS</h1>
+      <p>
+        If you can see this on your phone, the pipeline works: GitHub to
+        Vercel to here.
+      </p>
+    </main>
+  );
+}
